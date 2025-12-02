9dce7a112a67a5739f8b9634d9f53be408a355d0
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-12-02 21:30:46 +0600

    Integrate database configuration in server

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import config from './config/envConfig.js';
 
-dotenv.config();
 
 const app = express();
+connectDB();
 app.use(express.json());
-app.use(cors());
+app.use(cors(config.CORS));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
