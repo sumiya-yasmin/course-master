d831a9891a8b6ba806a8e6e21ea9a2aebafb87bb
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-12-03 22:36:41 +0600

    Add register, login api

---
+++ client/src/api/auth.ts
@@ -0,0 +1,13 @@
+import { AuthResponse, LoginInput, RegisterInput } from "../lib/schema/authSchema";
+import apiClient from "./axios";
+
+
+export const loginUserApi = async (data: LoginInput): Promise<AuthResponse> => {
+  const response = await apiClient.post<AuthResponse>('/auth/login', data);
+  return response.data;
+};
+
+export const registerUserApi = async (data: RegisterInput): Promise<AuthResponse> => {
+  const response = await apiClient.post<AuthResponse>('/auth/register', data);
+  return response.data;
+};
