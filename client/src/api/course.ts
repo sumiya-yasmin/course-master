e84a43e3870783a2546884f680ac24a0c123fa0c
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-12-04 00:14:51 +0600

    Add courses api

---
+++ client/src/api/course.ts
@@ -0,0 +1,18 @@
+import { Course, CourseParams, CourseResponse } from "../types/course";
+import apiClient from "./axios";
+
+export const getAllCoursesApi = async (params?: CourseParams) => {
+  const response = await apiClient.get<CourseResponse>('/courses', { params });
+  return response.data;
+};
+
+export const getCourseByIdApi = async (id: string) => {
+  const response = await apiClient.get<Course>(`/courses/${id}`);
+  return response.data;
+};
+
+
+export const enrollCourseApi = async (courseId: string) => {
+  const response = await apiClient.post('/enrollments', { courseId });
+  return response.data;
+};
