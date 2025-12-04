1705150bc1c4be4a130a6654da4642ee91183997
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-12-05 00:48:32 +0600

    Add submission model

---
+++ server/src/models/Submission.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const submissionSchema = new mongoose.Schema({
+  student: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User', 
+    required: true 
+  },
+  course: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Course', 
+    required: true 
+  },
+  type: {
+    type: String,
+    enum: ['assignment', 'quiz'],
+    required: true
+  },
+  content: { type: String },
+  score: { type: Number },
+}, { timestamps: true });
+
+export default mongoose.model('Submission', submissionSchema);
