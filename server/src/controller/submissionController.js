3985f77f68eddbe7cdecb2fe8288546ec6f5dc79
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-12-05 00:49:09 +0600

    Add submission controller

---
+++ server/src/controller/submissionController.js
@@ -0,0 +1,29 @@
+import * as submissionService from '../services/submissionServices.js';
+
+export const createSubmission = async (req, res) => {
+  try {
+    const { courseId, type, content, score } = req.body;
+    
+    const submission = await submissionService.createSubmissionService({
+      userId: req.user._id,
+      courseId,
+      type,
+      content,
+      score
+    });
+
+    res.status(201).json(submission);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+
+export const getAllSubmissions = async (req, res) => {
+  try {
+    const submissions = await submissionService.getAllSubmissionsService();
+    res.status(200).json(submissions);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
