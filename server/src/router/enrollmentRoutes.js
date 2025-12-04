14df635439679e5639efbee72447a194387bb24a
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-12-04 13:08:08 +0600

    Add getAllEnrollment routes

--- server/src/router/enrollmentRoutes.js
+++ server/src/router/enrollmentRoutes.js
@@ -1,14 +1,18 @@
 import express from 'express';
 import { 
   enrollStudent, 
+  getAllEnrollments, 
   getMyCourses, 
   updateProgress 
 } from '../controller/enrollmentController.js';
+import { admin } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/', enrollStudent);              
 router.get('/my-courses', getMyCourses);      
 router.post('/progress', updateProgress);      
+
+router.get('/', admin, getAllEnrollments); 
 
 export default router;
