27ce8315c09f180490707ca740da4f68289def43
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-12-05 00:48:46 +0600

    Add submission services

---
+++ server/src/services/submissionServices.js
@@ -0,0 +1,19 @@
+import Submission from '../models/Submission.js';
+
+export const createSubmissionService = async ({ userId, courseId, type, content, score }) => {
+  const submission = await Submission.create({
+    student: userId,
+    course: courseId,
+    type,
+    content,
+    score
+  });
+  return submission;
+};
+
+export const getAllSubmissionsService = async () => {
+  return await Submission.find()
+    .populate('student', 'name email')
+    .populate('course', 'title')
+    .sort({ createdAt: -1 });
+};
