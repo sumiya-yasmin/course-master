cb6480837de1cf84e023d2daa14b7a9337b6dcb9
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-12-05 00:49:31 +0600

    Add submission routes

--- server/src/router/index.js
+++ server/src/router/index.js
@@ -2,10 +2,12 @@ import { protect } from '../middlewares/authMiddleware.js';
 import authRouter from './authRoutes.js'
 import courseRouter from './courseRoutes.js'
 import enrollmentRouter from './enrollmentRoutes.js'
+import submissionRouter from './submissionRoutes.js'
 const configureRouter = (app) => {
   app.use('/auth', authRouter);
   app.use('/courses', courseRouter);
   app.use('/enrollments', protect, enrollmentRouter);
+  app.use('/submissions', submissionRouter);
 };
 
 export default configureRouter;
---
+++ server/src/router/submissionRoutes.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import { createSubmission, getAllSubmissions } from '../controller/submissionController.js';
+import { protect, admin } from '../middlewares/authMiddleware.js';
+
+const router = express.Router();
+
+router.post('/', protect, createSubmission);      
+router.get('/', protect, admin, getAllSubmissions); 
+
+export default router;
